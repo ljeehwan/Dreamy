51523bea1ee6944e891ca652f667a8b87f937f40
ingorithm <ingu94@gmail.com>
2021-01-21 15:57:04 +0900

    S04P12A306-25 MODIFY board/create

--- Backend/src/main/java/com/ssafy/Dreamy/controller/BoardController.java
+++ Backend/src/main/java/com/ssafy/Dreamy/controller/BoardController.java
@@ -1,17 +1,25 @@
 package com.ssafy.Dreamy.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.Dreamy.model.BoardDto;
 import com.ssafy.Dreamy.model.service.BoardService;
 
 @CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/board")	// 매핑주소 변경가능
 public class BoardController {
 
 	public static final Logger logger = LoggerFactory.getLogger(BoardController.class);
@@ -21,5 +29,23 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 
-	
+	//////////게시물 등록///////////
+	@PostMapping("/create")	// 매핑주소 변경가능
+	public ResponseEntity<Map<String, Object>> create(@RequestBody BoardDto boardDto) {
+		Map<String, Object> resultMap = new HashMap<>();
+		
+		HttpStatus status = null;
+		try {
+			int ret = (int)boardService.create(boardDto);
+			// if
+			resultMap.put("message", SUCCESS);
+			status = HttpStatus.ACCEPTED;
+		} catch (Exception e) {
+			logger.error("게시물 등록 실패 : {}", e);
+			resultMap.put("message", e.getMessage());
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
+
 }
--- Backend/src/main/java/com/ssafy/Dreamy/model/service/BoardServiceImpl.java
+++ Backend/src/main/java/com/ssafy/Dreamy/model/service/BoardServiceImpl.java
@@ -1,11 +1,38 @@
 package com.ssafy.Dreamy.model.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.ssafy.Dreamy.model.BoardDto;
+import com.ssafy.Dreamy.model.mapper.BoardMapper;
+
+@Service
 public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private SqlSession sqlSession;
 	
+	@Override
+	public Object create(BoardDto boardDto) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		map.put("uid", boardDto.getUid());
+		map.put("boardType", boardDto.getBoardType());
+		map.put("title", boardDto.getTitle());
+		map.put("content", boardDto.getContent());
+		map.put("startDate", boardDto.getStartDate());
+		map.put("endDate", boardDto.getEndDate());
+		map.put("category", boardDto.getCategory());
+		map.put("imageUrl", boardDto.getImageUrl());
+		map.put("dateType", boardDto.getDateType());
+		map.put("totalDate", boardDto.getTotalDate());
+		map.put("running", boardDto.getRunning());
+		
+		return sqlSession.getMapper(BoardMapper.class).create(map);
+	}
+	
 }
+
