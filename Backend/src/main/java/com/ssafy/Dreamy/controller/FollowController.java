2742ff45dbb49019d5781a3774f84a00e7c07b45
multicampus <96missha@hanmail.net>
2021-01-22 15:54:19 +0900

    Closes S04P12A306-36 MODIFY BE/feature/followRelation

--- Backend/src/main/java/com/ssafy/Dreamy/controller/FollowController.java
+++ Backend/src/main/java/com/ssafy/Dreamy/controller/FollowController.java
@@ -11,10 +11,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.Dreamy.model.MemberDto;
 import com.ssafy.Dreamy.model.service.FollowService;
 
 @CrossOrigin(origins = { "http://localhost:3000" })
@@ -78,6 +81,29 @@ public class FollowController {
 		}
 		return new ResponseEntity<Map<String, Object>>(resultMap, status);
 	}
-	
+
 	////////// 팔로우 관계 검증 ///////////
+	@GetMapping
+	public ResponseEntity<Map<String, Object>> relationcheck(HttpServletRequest request, Model model) throws Exception {
+		Map<String, Object> resultMap = new HashMap<>();
+		HttpStatus status = null;
+
+		String clickUser = request.getParameter("login_id");
+		String followedUser = request.getParameter("target_id");
+
+		int user_id = Integer.parseInt(clickUser);
+		int target_id = Integer.parseInt(followedUser);
+
+		if (followservice.followcheck(user_id, target_id)) {
+			System.out.println("--친구관계성립");
+			resultMap.put("message", SUCCESS);
+			status = HttpStatus.ACCEPTED;
+		} else {
+			System.out.println("--친구관계아니다");
+			resultMap.put("mwssage", FAIL);
+			status = HttpStatus.UNAUTHORIZED;
+
+		}
+		return new ResponseEntity<Map<String, Object>>(resultMap, status);
+	}
 }
