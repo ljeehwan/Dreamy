6688041d1b2bec42dfee38e4d93cdcf584e9c192
multicampus <96missha@hanmail.net>
2021-01-27 10:36:21 +0900

    S04P12A306-71 MODIFY

--- Frontend/src/components/LoginUser.vue
+++ Frontend/src/components/LoginUser.vue
@@ -17,48 +17,50 @@
       </v-btn>
     </template>
     <v-card width="300">
-        <v-container grid-list-md>
-            <v-layout row wrap>
-                <v-flex xs4>
-                    <v-avatar class="mx-1" size="64">  
-                    <v-icon size="64"> 
-                            mdi-account-circle
-                        </v-icon>
-                    </v-avatar>
-                </v-flex>
-                <v-flex xs8>
-                    <v-card-text>
-                    {{getUsername}} 님<br><hr>
-                    {{getEmail}} </v-card-text>
-                </v-flex>
-            </v-layout>
-                <v-btn @click="moveToMyPage" text style="width:180px;">MyPage</v-btn>
-            <br>
-            <v-btn target="_blank" text @click="logout" style="width:180px; color:red;">Logout</v-btn>
-        </v-container>
+      <v-container grid-list-md>
+        <v-layout row wrap>
+          <v-flex xs4>
+            <v-avatar class="mx-1" size="64">
+              <v-icon size="64">
+                mdi-account-circle
+              </v-icon>
+            </v-avatar>
+          </v-flex>
+          <v-flex xs8>
+            <v-card-text>
+              {{ getUsername }} 님<br />
+              <hr />
+              {{ getEmail }}
+            </v-card-text>
+          </v-flex>
+        </v-layout>
+          <v-btn target="_blank" @click="moveToMyPage" text style="width:180px;" 
+            >MyPage</v-btn
+          >
+        <br />
+        <v-btn
+          target="_blank"
+          text
+          @click="logout"
+          style="width:180px; color:red;"
+          >Logout</v-btn
+        >
+      </v-container>
     </v-card>
   </v-menu>
 </template>
 
+
 <script>
-import {router} from "../routes"
-
+import {router} from '@/routes'
 export default {
-    data: function () {
-        return {
-
-        }
+  computed: {
+    getEmail() {
+      // 렌더링 시점때문에 mapgetters 사용시 오류가 나므로 이렇게 쓰기
+      return this.$store.getters.getEmail;
     },
-    computed:{
-        getEmail(){         // 렌더링 시점때문에 mapgetters 사용시 오류가 나므로 이렇게 쓰기
-            return this.$store.getters.getEmail;
-        },
-        getUsername(){
-            return this.$store.getters.getUsername;
-        },
-        getLogintype(){
-             return this.$store.getters.getLogintype;
-        }
+    getUsername() {
+      return this.$store.getters.getUsername;
     },
     getLogintype() {
       return this.$store.getters.getLogintype;
@@ -87,8 +89,17 @@ export default {
       }
       this.$store.dispatch("logout");
     },
+    moveToMyPage () {
+            // 원래는 다른사람 이름을 받아오겠지만, 마이페이지니까 그냥 이름 준다
+            const name = this.$store.getters.getUsername
+            this.$store.dispatch('GET_TARGET', name)
+            router.push('/user/mypage/')
+
+        },
+
   },
 };
 </script>
+
 
 <style></style>
