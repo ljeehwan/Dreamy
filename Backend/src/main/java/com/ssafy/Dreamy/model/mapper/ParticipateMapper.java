94fe1352039f858992deac1ec2174f1214d9fd36
ljeehwan <ljeehwan@naver.com>
2021-02-08 17:26:01 +0900

    S04P13A306-60 ADD BE/feature/participate

--- Backend/src/main/java/com/ssafy/Dreamy/model/mapper/ParticipateMapper.java
+++ Backend/src/main/java/com/ssafy/Dreamy/model/mapper/ParticipateMapper.java
@@ -1,5 +1,7 @@
 package com.ssafy.Dreamy.model.mapper;
 
+import java.util.Map;
+
 public interface ParticipateMapper {
-	
+	public int addParticipant(Map<String, Object> map) throws Exception;
 }
--- Backend/src/main/java/com/ssafy/Dreamy/model/service/ParticipateServiceImpl.java
+++ Backend/src/main/java/com/ssafy/Dreamy/model/service/ParticipateServiceImpl.java
@@ -1,5 +1,28 @@
 package com.ssafy.Dreamy.model.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ssafy.Dreamy.model.mapper.ParticipateMapper;
+
 public class ParticipateServiceImpl implements ParticipateService {
+
+	@Autowired
+	SqlSession sqlSession;
+	
+	@Override
+	public int addParticipant(int uid, int pid, int successDate) throws Exception {
+		// TODO Auto-generated method stub
+		Map<String, Object> map = new HashMap<>();
+		
+		map.put("uid", uid);
+		map.put("pid", pid);
+		map.put("successDate", successDate);
+		
+		return sqlSession.getMapper(ParticipateMapper.class).addParticipant(map);
+	}
 
 }
